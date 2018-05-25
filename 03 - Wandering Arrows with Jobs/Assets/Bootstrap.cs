0354fc89baa3159c22f50425f81ce0a2c445fd3f
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-05-25 20:22:39 +0300

    done

---
+++ 03 - Wandering Arrows with Jobs/Assets/Bootstrap.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public static class Bootstrap
+{
+    private static EntityManager _entityManager;
+    private static MeshInstanceRenderer _arrowRenderer;
+    private static EntityArchetype _arrowArchetype;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initialize()
+    {
+        _entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        _arrowArchetype = _entityManager.CreateArchetype(
+            typeof(TransformMatrix),
+            typeof(Position),
+            typeof(Heading),
+            typeof(MoveSpeed),
+            typeof(MoveForward),
+            typeof(HeadingTarget),
+            typeof(RandomizeHeadingTarget)
+        );
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void InitializeWithScene()
+    {
+        _arrowRenderer = GameObject.FindObjectOfType<MeshInstanceRendererComponent>().Value;
+
+        CreateArrowsWithRandomHeadings(count: 10);
+    }
+
+    private static void CreateArrowsWithRandomHeadings(int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            CreateArrow(heading: Random.onUnitSphere);
+        }
+    }
+
+    private static void CreateArrow(float3 heading)
+    {
+        var arrow = _entityManager.CreateEntity(_arrowArchetype);
+
+        _entityManager.SetComponentData(arrow, new Position {Value = new float3(0, 0, 0)});
+        _entityManager.SetComponentData(arrow, new Heading {Value = heading});
+        _entityManager.SetComponentData(arrow, new MoveSpeed {speed = 1});
+        _entityManager.SetComponentData(arrow, new HeadingTarget {Value = heading});
+
+        _entityManager.AddSharedComponentData(arrow, _arrowRenderer);
+    }
+}
---
+++ 03 - Wandering Arrows with Jobs/Assets/LerpHeadingTowardsHeadingTargetJobSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class LerpHeadingTowardsHeadingTargetJobSystem : JobComponentSystem
+{
+    [ComputeJobOptimization]
+    private struct HeadingTargetHeading : IJobProcessComponentData<HeadingTarget, Heading>
+    {
+        public void Execute([ReadOnly] ref HeadingTarget headingTarget, ref Heading heading)
+        {
+            heading.Value = math.lerp(heading.Value, headingTarget.Value, 0.01f);
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new HeadingTargetHeading();
+        return job.Schedule(this, 64, inputDeps);
+    }
+}
