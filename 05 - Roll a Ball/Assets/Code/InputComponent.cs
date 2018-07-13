0823b29f2376a60498d73ee1b04cb8ec838ecc2d
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-07-13 18:14:15 +0300

    basic logic implemented

---
+++ 05 - Roll a Ball/Assets/Code/InputComponent.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+    public float Horizontal;
+    public float Vertical;
+}
---
+++ 05 - Roll a Ball/Assets/Code/InputSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+public class InputSystem : ComponentSystem
+{
+    private struct Filter
+    {
+        public int Length;
+        [WriteOnly] public ComponentArray<InputComponent> InputComponents;
+    }
+
+    [Inject] private Filter _matched;
+
+    protected override void OnUpdate()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+
+        for (var i = 0; i < _matched.Length; i++)
+        {
+            var input = _matched.InputComponents[i];
+            input.Horizontal = horizontalInput;
+            input.Vertical = verticalInput;
+        }
+    }
+}
