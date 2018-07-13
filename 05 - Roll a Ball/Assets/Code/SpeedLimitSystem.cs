3abedca2132fd32c4978f72e34ee70826c5ca6bc
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-07-13 18:22:39 +0300

    speed limit implemented

---
+++ 05 - Roll a Ball/Assets/Code/SpeedLimitSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class SpeedLimitSystem : ComponentSystem
+{
+    private struct Filter
+    {
+        public int Length;
+        public ComponentArray<SpeedLimitComponent> SpeedLimits;
+        public ComponentArray<Rigidbody> Rigidbodies;
+    }
+
+    [Inject] private Filter _matched;
+
+    protected override void OnUpdate()
+    {
+        for (var i = 0; i < _matched.Length; i++)
+        {
+            var speedLimit = _matched.SpeedLimits[i].Value;
+            var rigidbody = _matched.Rigidbodies[i];
+
+            if (rigidbody.velocity.magnitude > speedLimit)
+            {
+                rigidbody.velocity = rigidbody.velocity.normalized * speedLimit;
+            }
+        }
+    }
+}
